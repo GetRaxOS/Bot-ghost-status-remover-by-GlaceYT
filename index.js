6a888a74e73b64ecf14520ba548d1a9562ab9be7
GetRaxOS <marekkucharek133@gmail.com>
2024-10-12 10:54:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["<:Management:1287084621590429726> Watching Rax"];
+const statusMessages = ["🎃 Watching Rax 🎃"];
 const statusTypes = [ 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
